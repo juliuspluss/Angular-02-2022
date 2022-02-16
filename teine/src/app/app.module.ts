09d9ad5e45f1456b52d09dc52928d0b7a73dc7ef
juliuspluss <99732420+juliuspluss@users.noreply.github.com>
2022-02-16 17:49:31 +0200

    Angular 1. kodune töö

--- teine/src/app/app.module.ts
+++ teine/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AvalehtComponent } from './avaleht/avaleht.component';
-import { OstukorvComponent } from './ostukorv/ostukorv.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AvalehtComponent,
-    OstukorvComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
