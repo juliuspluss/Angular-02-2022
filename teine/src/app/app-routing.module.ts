c3d71caa33ea80ae5bd233a747b6040d7e50ba0e
juliuspluss <99732420+juliuspluss@users.noreply.github.com>
2022-02-17 11:27:19 +0200

    Angular

--- teine/src/app/app-routing.module.ts
+++ teine/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NewsComponent } from './news/news.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path "", component: HomeComponent },
+  { path "news", component: NewsComponent },
+  { path "contact", component: ContactComponent },
+  { path "about", component: AboutComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
