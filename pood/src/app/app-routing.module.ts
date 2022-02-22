b1c9515276f505560534a820f5ba025ba6c34802
juliuspluss <99732420+juliuspluss@users.noreply.github.com>
2022-02-22 16:52:57 +0200

    uus pood

---
+++ pood/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AvalehtComponent } from './avaleht/avaleht.component';
+import { OstukorvComponent } from './ostukorv/ostukorv.component';
+
+const routes: Routes = [
+  { path: "", component: AvalehtComponent},
+  { path: "", component: OstukorvComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
