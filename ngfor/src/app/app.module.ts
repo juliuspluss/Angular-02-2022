c94d1fb745f43bdde2ace60f52524057286a3534
juliuspluss <99732420+juliuspluss@users.noreply.github.com>
2022-03-01 14:39:36 +0200

    Angular ngFor

---
+++ ngfor/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CatComponent } from './cat/cat.component';
+import { DogComponent } from './dog/dog.component';
+import { MouseComponent } from './mouse/mouse.component';
+import { RabbitComponent } from './rabbit/rabbit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CatComponent,
+    DogComponent,
+    MouseComponent,
+    RabbitComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ngfor/src/app/rabbit/rabbit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rabbit',
+  templateUrl: './rabbit.component.html',
+  styleUrls: ['./rabbit.component.css']
+})
+export class RabbitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
