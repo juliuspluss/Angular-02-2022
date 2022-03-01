88d24174e4a3beb7195981f6848228338583fbb1
juliuspluss <99732420+juliuspluss@users.noreply.github.com>
2022-03-01 13:03:50 +0200

    Angular 1. märts

---
+++ minishop/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MeeskondComponent } from './meeskond/meeskond.component';
+import { PoedComponent } from './poed/poed.component';
+import { TootedComponent } from './tooted/tooted.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeeskondComponent,
+    PoedComponent,
+    TootedComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ minishop/src/app/meeskond/meeskond.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meeskond',
+  templateUrl: './meeskond.component.html',
+  styleUrls: ['./meeskond.component.css']
+})
+export class MeeskondComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
